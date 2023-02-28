12816dbe645ef967efa3f387774750e9a46ac9dd
Wencong Liu <liuwenclever@163.com>
2023-02-28 15:54:04 +0800

    [TS] Refract the downstream package of TieredStore

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/tieredstore/downstream/TieredStoreReaderImpl.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/tieredstore/downstream/TieredStoreReaderImpl.java
@@ -17,7 +17,8 @@ public class TieredStoreReaderImpl implements TieredStoreReader {
 
     private final SingleChannelTierClientFactory clientFactory;
 
-    public TieredStoreReaderImpl(int numInputChannels, SingleChannelTierClientFactory clientFactory) {
+    public TieredStoreReaderImpl(
+            int numInputChannels, SingleChannelTierClientFactory clientFactory) {
         this.numInputChannels = numInputChannels;
         this.singleChannelReaders = new SingleChannelReader[numInputChannels];
         this.clientFactory = clientFactory;
@@ -27,14 +28,14 @@ public class TieredStoreReaderImpl implements TieredStoreReader {
     public void setup() throws IOException {
         for (int i = 0; i < numInputChannels; ++i) {
             singleChannelReaders[i] = new SingleChannelReaderImpl(clientFactory);
+            singleChannelReaders[i].setup();
         }
     }
 
     @Override
-    public Optional<InputChannel.BufferAndAvailability>
-            getNextBuffer(InputChannel inputChannel) throws IOException, InterruptedException {
-        return singleChannelReaders[inputChannel.getChannelIndex()].getNextBuffer(
-                inputChannel);
+    public Optional<InputChannel.BufferAndAvailability> getNextBuffer(InputChannel inputChannel)
+            throws IOException, InterruptedException {
+        return singleChannelReaders[inputChannel.getChannelIndex()].getNextBuffer(inputChannel);
     }
 
     @Override
