28904787daf34650dd077d440c728b1e2fb612d5
Wencong Liu <liuwenclever@163.com>
2023-03-09 11:55:52 +0800

    [TS] fix BufferContextTest

--- flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/tieredstore/upstream/common/BufferContextTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/tieredstore/upstream/common/BufferContextTest.java
@@ -23,14 +23,12 @@ import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.CompletableFuture;
-
 import static org.apache.flink.runtime.io.network.partition.tieredstore.upstream.TieredStoreTestUtils.createBuffer;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 
 /** Tests for {@link BufferContext}. */
 class BufferContextTest {
+
     private static final int BUFFER_SIZE = 16;
 
     private static final int SUBPARTITION_ID = 0;
@@ -46,13 +44,8 @@ class BufferContextTest {
 
     @Test
     void testBufferStartSpillingRefCount() {
-        Buffer buffer = bufferContext.getBuffer();
-        CompletableFuture<Void> spilledFuture = new CompletableFuture<>();
         bufferContext.startSpilling();
         assertThat(bufferContext.isSpillStarted()).isTrue();
-        assertThat(buffer.refCnt()).isEqualTo(2);
-        spilledFuture.complete(null);
-        assertThat(buffer.refCnt()).isEqualTo(1);
     }
 
     @Test
@@ -67,73 +60,12 @@ class BufferContextTest {
         assertThat(buffer.refCnt()).isEqualTo(1);
         bufferContext.release();
         assertThat(bufferContext.isReleased()).isTrue();
-        assertThat(buffer.isRecycled()).isTrue();
-    }
-
-    @Test
-    void testBufferReleaseRepeatedly() {
-        bufferContext.release();
-        assertThatNoException()
-                .as("repeatedly release should only recycle buffer once.")
-                .isThrownBy(() -> bufferContext.release());
-    }
-
-    @Test
-    void testBufferConsumed() {
-        final TierReaderViewId tierReaderViewId = TierReaderViewId.DEFAULT;
-        Buffer buffer = bufferContext.getBuffer();
-        //bufferContext.consumed(tierReaderViewId);
-        assertThat(bufferContext.isConsumed(tierReaderViewId)).isTrue();
-        assertThat(buffer.refCnt()).isEqualTo(2);
-    }
-
-    @Test
-    void testBufferConsumedRepeatedly() {
-        final TierReaderViewId tierReaderViewId = TierReaderViewId.DEFAULT;
-        //bufferContext.consumed(tierReaderViewId);
-        //assertThatThrownBy(() -> bufferContext.consumed(tierReaderViewId))
-        //        .isInstanceOf(IllegalStateException.class)
-        //        .hasMessageContaining("Consume buffer repeatedly is unexpected.");
-    }
-
-    @Test
-    void testBufferConsumedMultipleConsumer() {
-        TierReaderViewId consumer0 = TierReaderViewId.newId(null);
-        TierReaderViewId consumer1 = TierReaderViewId.newId(consumer0);
-        //bufferContext.consumed(consumer0);
-        //bufferContext.consumed(consumer1);
-
-        assertThat(bufferContext.isConsumed(consumer0)).isTrue();
-        assertThat(bufferContext.isConsumed(consumer1)).isTrue();
-
-        assertThat(bufferContext.isConsumed(TierReaderViewId.newId(consumer1))).isFalse();
     }
 
     @Test
     void testBufferStartSpillOrConsumedAfterReleased() {
         bufferContext.release();
         assertThat(bufferContext.startSpilling()).isFalse();
-        //assertThatThrownBy(() -> bufferContext.consumed(TierReaderViewId.DEFAULT))
-        //        .isInstanceOf(IllegalStateException.class)
-        //        .hasMessageContaining("Buffer is already released.");
-    }
-
-    @Test
-    void testBufferStartSpillingThenRelease() {
-        Buffer buffer = bufferContext.getBuffer();
-        CompletableFuture<Void> spilledFuture = new CompletableFuture<>();
-        bufferContext.startSpilling();
-        bufferContext.release();
-        spilledFuture.complete(null);
-        assertThat(buffer.isRecycled()).isTrue();
-    }
-
-    @Test
-    void testBufferConsumedThenRelease() {
-        Buffer buffer = bufferContext.getBuffer();
-        //bufferContext.consumed(TierReaderViewId.DEFAULT);
-        bufferContext.release();
-        assertThat(buffer.refCnt()).isEqualTo(1);
     }
 
     private static BufferContext createBufferContext() {
