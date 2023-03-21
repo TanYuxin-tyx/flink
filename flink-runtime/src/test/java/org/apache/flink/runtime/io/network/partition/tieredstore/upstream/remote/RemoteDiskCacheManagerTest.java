2f948a551bdc875378f0b09ec21d7b9c7f5b5e50
Wencong Liu <liuwenclever@163.com>
2023-03-21 17:12:12 +0800

    [TS] Optimize the SubpartitionRemoteCacheManager

--- flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/tieredstore/upstream/remote/RemoteDiskCacheManagerTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/tieredstore/upstream/remote/RemoteDiskCacheManagerTest.java
@@ -18,13 +18,18 @@
 
 package org.apache.flink.runtime.io.network.partition.tieredstore.upstream.remote;
 
+import org.apache.flink.api.common.JobID;
 import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.apache.flink.runtime.io.network.buffer.BufferPool;
 import org.apache.flink.runtime.io.network.buffer.NetworkBufferPool;
+import org.apache.flink.runtime.io.network.partition.ResultPartitionID;
 import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.TieredStoreTestUtils;
 import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.CacheFlushManager;
 import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.TieredStoreMemoryManager;
 import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.UpstreamTieredStoreMemoryManager;
+import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.file.PartitionFileManager;
+import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.file.PartitionFileManagerImpl;
+import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.file.PartitionFileType;
 import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.tier.remote.RemoteCacheManager;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -44,82 +49,74 @@ class RemoteDiskCacheManagerTest {
 
     private static final int NUM_SUBPARTITIONS = 3;
 
-    private final int poolSize = 10;
+    private static final int BUFFER_SIZE = Integer.BYTES * 3;
 
-    private int bufferSize = Integer.BYTES;
+    private static final int POOL_SIZE = 10;
 
     private TemporaryFolder tmpFolder;
+
+    private PartitionFileManager partitionFileManager;
 
     @BeforeEach
     void setup() throws IOException {
         this.tmpFolder = TemporaryFolder.builder().build();
         tmpFolder.create();
+        this.partitionFileManager =
+                new PartitionFileManagerImpl(
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        NUM_SUBPARTITIONS,
+                        JobID.generate(),
+                        new ResultPartitionID(),
+                        tmpFolder.newFolder().getPath());
     }
 
     @Test
     void testAppendMarkBufferFinished() throws Exception {
-        bufferSize = Integer.BYTES * 3;
-        NetworkBufferPool networkBufferPool = new NetworkBufferPool(NUM_BUFFERS, bufferSize);
-        BufferPool bufferPool = networkBufferPool.createBufferPool(poolSize, poolSize);
-        TieredStoreMemoryManager tieredStoreMemoryManager =
-                new UpstreamTieredStoreMemoryManager(
-                        bufferPool,
-                        getTierExclusiveBuffers(),
-                        NUM_SUBPARTITIONS,
-                        new CacheFlushManager());
-        RemoteCacheManager cacheDataManager = createDfsCacheDataManager(tieredStoreMemoryManager);
-
+        RemoteCacheManager cacheDataManager = createRemoteCacheDataManager();
         cacheDataManager.append(createRecord(0), 0, Buffer.DataType.DATA_BUFFER, false);
         cacheDataManager.append(createRecord(1), 0, Buffer.DataType.DATA_BUFFER, false);
         cacheDataManager.append(createRecord(2), 0, Buffer.DataType.DATA_BUFFER, false);
-
         cacheDataManager.append(createRecord(3), 0, Buffer.DataType.DATA_BUFFER, false);
     }
 
     @Test
     void testStartSegmentInMultiTimes() throws Exception {
-        bufferSize = Integer.BYTES * 3;
-        NetworkBufferPool networkBufferPool = new NetworkBufferPool(NUM_BUFFERS, bufferSize);
-        BufferPool bufferPool = networkBufferPool.createBufferPool(poolSize, poolSize);
-        TieredStoreMemoryManager tieredStoreMemoryManager =
-                new UpstreamTieredStoreMemoryManager(
-                        bufferPool,
-                        getTierExclusiveBuffers(),
-                        NUM_SUBPARTITIONS,
-                        new CacheFlushManager());
-        RemoteCacheManager cacheDataManager = createDfsCacheDataManager(tieredStoreMemoryManager);
+        RemoteCacheManager cacheDataManager = createRemoteCacheDataManager();
         cacheDataManager.startSegment(0, 0);
         assertThrows(IllegalStateException.class, () -> cacheDataManager.startSegment(0, 0));
     }
 
     @Test
     void testFinishSegmentInMultiTimes() throws Exception {
-        bufferSize = Integer.BYTES * 3;
-        NetworkBufferPool networkBufferPool = new NetworkBufferPool(NUM_BUFFERS, bufferSize);
-        BufferPool bufferPool = networkBufferPool.createBufferPool(poolSize, poolSize);
+        RemoteCacheManager cacheDataManager = createRemoteCacheDataManager();
+        cacheDataManager.startSegment(0, 0);
+        cacheDataManager.finishSegment(0, 0);
+        assertThrows(IllegalStateException.class, () -> cacheDataManager.finishSegment(0, 1));
+    }
+
+    private RemoteCacheManager createRemoteCacheDataManager() throws Exception {
+        BufferPool localBufferPool =
+                new NetworkBufferPool(NUM_BUFFERS, BUFFER_SIZE)
+                        .createBufferPool(POOL_SIZE, POOL_SIZE);
         TieredStoreMemoryManager tieredStoreMemoryManager =
                 new UpstreamTieredStoreMemoryManager(
-                        bufferPool,
+                        localBufferPool,
                         getTierExclusiveBuffers(),
                         NUM_SUBPARTITIONS,
                         new CacheFlushManager());
-        RemoteCacheManager cacheDataManager = createDfsCacheDataManager(tieredStoreMemoryManager);
-
-        cacheDataManager.startSegment(0, 0);
-        cacheDataManager.finishSegment(0, 0);
-        assertThrows(IllegalStateException.class, () -> cacheDataManager.finishSegment(0, 0));
-    }
-
-    private RemoteCacheManager createDfsCacheDataManager(
-            TieredStoreMemoryManager tieredStoreMemoryManager) throws Exception {
         RemoteCacheManager cacheDataManager =
                 new RemoteCacheManager(
                         NUM_SUBPARTITIONS,
-                        bufferSize,
+                        BUFFER_SIZE,
                         tieredStoreMemoryManager,
                         new CacheFlushManager(),
                         null,
-                        null);
+                        partitionFileManager.createPartitionFileWriter(
+                                PartitionFileType.PRODUCER_HASH));
         cacheDataManager.setOutputMetrics(TieredStoreTestUtils.createTestingOutputMetrics());
         return cacheDataManager;
     }
