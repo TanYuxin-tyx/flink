e39b1941372809b6cf5620f01dcecf6e5ca68e94
Wencong Liu <liuwenclever@163.com>
2023-03-28 14:38:50 +0800

    [TS] fix the bug for BufferAccumulator

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/tieredstore/upstream/common/UpstreamTieredStoreMemoryManager.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/tieredstore/upstream/common/UpstreamTieredStoreMemoryManager.java
@@ -24,6 +24,9 @@ import org.apache.flink.runtime.io.network.partition.tieredstore.upstream.Tiered
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.apache.flink.runtime.io.network.partition.tieredstore.upstream.common.TieredStoreUtils.needFlushCacheBuffers;
@@ -46,6 +49,8 @@ public class UpstreamTieredStoreMemoryManager implements TieredStoreMemoryManage
 
     private final int numTotalExclusiveBuffers;
 
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
     public UpstreamTieredStoreMemoryManager(
             BufferPool bufferPool,
             Map<TieredStoreMode.TieredType, Integer> tierExclusiveBuffers,
@@ -58,6 +63,11 @@ public class UpstreamTieredStoreMemoryManager implements TieredStoreMemoryManage
         this.numSubpartitions = numSubpartitions;
         this.numTotalExclusiveBuffers =
                 tierExclusiveBuffers.values().stream().mapToInt(i -> i).sum();
+        executor.scheduleAtFixedRate(
+                UpstreamTieredStoreMemoryManager.this::checkNeedTriggerFlushCachedBuffers,
+                10,
+                50,
+                TimeUnit.MILLISECONDS);
     }
 
     public BufferPool getBufferPool() {
