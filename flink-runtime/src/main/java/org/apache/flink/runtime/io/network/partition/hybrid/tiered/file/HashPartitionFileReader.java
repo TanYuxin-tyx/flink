2b4ad30a5909f25d8f1d84c8989c7e4e1d6ce773
Wencong Liu <liuwenclever@163.com>
2023-06-20 12:27:39 +0800

    [TS] Implement the HashPartitionFileReader and RemoteTierConsumerAgent

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/file/HashPartitionFileReader.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/file/HashPartitionFileReader.java
@@ -18,31 +18,56 @@
 
 package org.apache.flink.runtime.io.network.partition.hybrid.tiered.file;
 
+import org.apache.flink.api.common.JobID;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.core.memory.MemorySegment;
 import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.apache.flink.runtime.io.network.buffer.BufferRecycler;
 import org.apache.flink.runtime.io.network.partition.BufferReaderWriterUtil;
+import org.apache.flink.runtime.io.network.partition.hybrid.tiered.common.TieredStorageIdMappingUtils;
 import org.apache.flink.runtime.io.network.partition.hybrid.tiered.common.TieredStoragePartitionId;
 import org.apache.flink.runtime.io.network.partition.hybrid.tiered.common.TieredStorageSubpartitionId;
+import org.apache.flink.util.ExceptionUtils;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channel;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.apache.flink.runtime.io.network.partition.hybrid.tiered.common.TieredStorageUtils.generateNewSegmentPath;
+import static org.apache.flink.runtime.io.network.partition.hybrid.tiered.common.TieredStorageUtils.getBaseSubpartitionPath;
 
 /** THe implementation of {@link PartitionFileReader} with hash logic. */
 public class HashPartitionFileReader implements PartitionFileReader {
 
     private final ByteBuffer reusedHeaderBuffer = BufferReaderWriterUtil.allocatedHeaderBuffer();
 
-    private final Map<TieredStoragePartitionId, Map<TieredStorageSubpartitionId, Channel>>
-            openedFileChannels = new HashMap<>();
+    private final Map<
+                    TieredStoragePartitionId,
+                    Map<TieredStorageSubpartitionId, Tuple2<ReadableByteChannel, Integer>>>
+            openedChannels = new HashMap<>();
 
     private final String basePath;
 
-    HashPartitionFileReader(String basePath) {
+    private final JobID jobID;
+
+    private final Boolean isUpstreamBroadcast;
+
+    private FileSystem fileSystem;
+
+    HashPartitionFileReader(String basePath, JobID jobID, Boolean isUpstreamBroadCast) {
         this.basePath = basePath;
+        this.jobID = jobID;
+        this.isUpstreamBroadcast = isUpstreamBroadCast;
+        try {
+            this.fileSystem = new Path(basePath).getFileSystem();
+        } catch (IOException e) {
+            ExceptionUtils.rethrow(e, "Failed to initialize the FileSystem.");
+        }
     }
 
     @Override
@@ -54,19 +79,40 @@ public class HashPartitionFileReader implements PartitionFileReader {
             MemorySegment segment,
             BufferRecycler recycler)
             throws IOException {
-        Channel fileChannel = openedFileChannels
-                .computeIfAbsent(partitionId, ignore -> new HashMap<>())
-                .getOrDefault(subpartitionId, null);
-
-
-        //Buffer buffer = null;
-        //try {
-        //    buffer = readFromByteChannel(fileChannel, reusedHeaderBuffer, segment, recycler);
-        //} catch (IOException e) {
-        //    ExceptionUtils.rethrow(e, "Failed to read buffer.");
-        //}
-        //return buffer;
+        Tuple2<ReadableByteChannel, Integer> fileChannelAndSegmentId =
+                openedChannels
+                        .computeIfAbsent(partitionId, ignore -> new HashMap<>())
+                        .getOrDefault(subpartitionId, Tuple2.of(null, -1));
+        ReadableByteChannel channel = fileChannelAndSegmentId.f0;
+        if (channel == null || fileChannelAndSegmentId.f1 != segmentId) {
+            if (channel != null) {
+                channel.close();
+            }
+            channel = openNewChannel(partitionId, subpartitionId, segmentId);
+            openedChannels.get(partitionId).put(subpartitionId, Tuple2.of(channel, segmentId));
+        }
         return null;
+    }
+
+    private ReadableByteChannel openNewChannel(
+            TieredStoragePartitionId partitionId,
+            TieredStorageSubpartitionId subpartitionId,
+            int segmentId) {
+        String baseSubpartitionPath =
+                getBaseSubpartitionPath(
+                        jobID,
+                        TieredStorageIdMappingUtils.convertId(partitionId),
+                        subpartitionId.getSubpartitionId(),
+                        basePath,
+                        isUpstreamBroadcast);
+        Path currentSegmentPath = generateNewSegmentPath(baseSubpartitionPath, segmentId);
+        ReadableByteChannel channel = null;
+        try {
+            channel = Channels.newChannel(fileSystem.open(currentSegmentPath));
+        } catch (IOException e) {
+            ExceptionUtils.rethrow(e, "Failed to create file channel.");
+        }
+        return channel;
     }
 
     @Override
--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/tier/remote/RemoteTierConsumerAgent.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/tier/remote/RemoteTierConsumerAgent.java
@@ -1,11 +1,8 @@
 package org.apache.flink.runtime.io.network.partition.hybrid.tiered.tier.remote;
 
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.core.memory.MemorySegment;
 import org.apache.flink.core.memory.MemorySegmentFactory;
 import org.apache.flink.runtime.io.network.buffer.Buffer;
-import org.apache.flink.runtime.io.network.buffer.BufferBuilder;
-import org.apache.flink.runtime.io.network.buffer.BufferConsumer;
 import org.apache.flink.runtime.io.network.buffer.BufferHeader;
 import org.apache.flink.runtime.io.network.buffer.FreeingBufferRecycler;
 import org.apache.flink.runtime.io.network.buffer.NetworkBuffer;
@@ -19,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -116,34 +115,28 @@ public class RemoteTierConsumerAgent implements TierConsumerAgent {
     // ------------------------------------
 
     private Buffer readBuffer(InputStream inputStream) throws IOException {
+        ReadableByteChannel channel = Channels.newChannel(inputStream);
+
+        //
+
         headerBuffer.clear();
-        int bufferHeaderResult = inputStream.read(headerBuffer.array());
+        int bufferHeaderResult = channel.read(headerBuffer);
         if (bufferHeaderResult == -1) {
             throw new IOException("Empty header buffer is read from dfs.");
         }
+        headerBuffer.rewind();
         BufferHeader header = parseBufferHeader(headerBuffer);
-        ByteBuffer dataBuffer = ByteBuffer.wrap(new byte[header.getLength()]);
-        int dataBufferResult = inputStream.read(dataBuffer.array(), 0, header.getLength());
+        ByteBuffer dataBuffer = ByteBuffer.allocateDirect(header.getLength());
+        int dataBufferResult = channel.read(dataBuffer);
         if (dataBufferResult == -1) {
             throw new IOException("Empty data buffer is read from dfs.");
         }
         Buffer.DataType dataType = header.getDataType();
-        if (dataType.isBuffer()) {
-            BufferBuilder builder = storageMemoryManager.requestBufferBlocking(this);
-            BufferConsumer bufferConsumer = builder.createBufferConsumer();
-            Buffer buffer = bufferConsumer.build();
-            MemorySegment memorySegment = buffer.getMemorySegment();
-            memorySegment.put(0, dataBuffer.array(), 0, header.getLength());
-            return new NetworkBuffer(
-                    memorySegment,
-                    buffer.getRecycler(),
-                    dataType,
-                    header.isCompressed(),
-                    header.getLength());
-        } else {
-            MemorySegment memorySegment = MemorySegmentFactory.wrap(dataBuffer.array());
-            return new NetworkBuffer(
-                    memorySegment, FreeingBufferRecycler.INSTANCE, dataType, memorySegment.size());
-        }
+        return new NetworkBuffer(
+                MemorySegmentFactory.wrapOffHeapMemory(dataBuffer),
+                FreeingBufferRecycler.INSTANCE,
+                dataType,
+                header.isCompressed(),
+                header.getLength());
     }
 }
