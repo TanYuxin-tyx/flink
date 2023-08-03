fef024f9717f5e100d89776875ff50aa0e9e276c
Yuxin Tan <tanyuxinwork@gmail.com>
2023-08-03 15:48:22 +0800

    Revert "[TS] Modify the flush ratio"
    This reverts commit 94dde52f0f092d31b58a5405575f767bea4a18ec.

--- flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/TestingTieredStorageMemoryManager.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/TestingTieredStorageMemoryManager.java
@@ -95,11 +95,6 @@ public class TestingTieredStorageMemoryManager implements TieredStorageMemoryMan
     }
 
     @Override
-    public float numUsedRatio() {
-        return 0;
-    }
-
-    @Override
     public int numOwnerRequestedBuffer(Object owner) {
         return numOwnerRequestedBufferFunction.apply(owner);
     }
