b240f27cbdbe3df5e23a1eeab1a01626a3101ccf
Yuxin Tan <tanyuxinwork@gmail.com>
2023-08-15 10:35:23 +0800

    fix9

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/tier/disk/DiskIOScheduler.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/hybrid/tiered/tier/disk/DiskIOScheduler.java
@@ -169,7 +169,7 @@ public class DiskIOScheduler implements Runnable, BufferRecycler, NettyServicePr
         synchronized (lock) {
             checkState(!isReleased, "DiskIOScheduler is already released.");
             ScheduledSubpartitionReader scheduledSubpartitionReader;
-            if (shouldPrint && isBroadcast && !taskName.contains("500")) {
+            if (shouldPrint && isBroadcast) {
                 scheduledSubpartitionReader =
                         new ScheduledSubpartitionReader(
                                 subpartitionId, nettyConnectionWriter, true);
